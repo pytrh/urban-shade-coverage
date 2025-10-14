68b030f2d67f96e41e6fe9d0f24bb8413c436e76
Joe <zhaoyuan.song@etu.u-bordeaux.fr>
2025-10-14 14:24:17 +0200

    main.py

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
 import networkx as nx
 import osmnx as ox
+
+# [out:json][timeout:25];
+
+# // 1) Fetch Université de Bordeaux object
+# nwr["amenity"="university"]["name"~"Université de Bordeaux"]->.univ;
+
+# // 2) Turn it into an Area
+# (.univ;)->.univArea;
+
+# // Show the university outline
+# .univ out geom;
+
+# // 3) Buildings INSIDE the university area
+# (
+#   way["building"](area.univArea);
+#   relation["building"](area.univArea);
+# );
+# out geom;
+
+# // 4) Walkable areas INSIDE the university area
+# (
+#   way["highway"="footway"](area.univArea);
+#   way["footway"="sidewalk"](area.univArea);
+#   way["highway"="path"](area.univArea);
+#   way["highway"="pedestrian"](area.univArea);
+#   way["highway"="steps"](area.univArea);
+#   relation["highway"="pedestrian"](area.univArea);
+# );
+# out geom;t
